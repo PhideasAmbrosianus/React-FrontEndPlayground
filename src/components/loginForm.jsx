f85178cbbd47f109a4bdaaa70988738f79b12c94
PhideasAmbrosianus <102189571+PhideasAmbrosianus@users.noreply.github.com>
2022-06-16 09:44:44 -0400

    Examples with accessing DOM and autoFocus

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef(); //Creates a vDOM ref object
+  username = React.createRef(); //Creates a React reference we can apply to a DOM object
+
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
 
   handleSubmit = (e) => {
     e.preventDefault(); //Prevents default behaviour of this event
 
     // Call the server
     // const username = document.getElementById('username').value; // <- This works normally, but don't do this in REACT. React is all about the virtual DOM not the actual DOM
-    const username = this.username.current.value; // If you really need to access the DOM this is the method. There is a better way for Forms, learning this next.
+    //const username = this.username.current.value; // If you really need to access the DOM this is the method. There is a better way for Forms, learning this next.
     // Minimize use of refs - third party DOM libs, focus control, or animations. Don't overuse them.
+    // current = reference to DOM object
 
     console.log("Submitted");
   };
@@ -22,6 +27,7 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
+              autoFocus
               ref={this.username}
               id="username"
               type="text"
